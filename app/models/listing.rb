981f41324758aec39337f855700e27810c242cd3
Florian Kramer <floriankramer@gmx.de>
2014-10-19 21:12:34 +0100

    lecture 21

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,8 +1,13 @@
 class Listing < ActiveRecord::Base
 	Paperclip.options[:command_path] = 'C:\Program Files\ImageMagick-6.8.9-Q16'
-  	has_attached_file 					:image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
-  										:storage => :dropbox,
-    									:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+	if Rails.env.development?
+		has_attached_file 		:image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg"
+	else
+  		has_attached_file 		:image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.jpg",
+  								:storage => :dropbox,
+    							:dropbox_credentials => Rails.root.join("config/dropbox.yml")
+    							:path => ":style/:id_filename"
+    end
   	validates_attachment_content_type 	:image, :content_type => /\Aimage\/.*\Z/
 end
 
